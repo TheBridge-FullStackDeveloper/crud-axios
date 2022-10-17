50805f07d1d68f28512f689d8d6154165a4cc4ac
SofiaPinilla <contact@sofiapinilla.com>
2022-10-17 11:53:58 +0200

    convirtiendo promesas en async/ await

--- main.js
+++ main.js
@@ -14,47 +14,76 @@ axios
   })
   .catch((err) => console.error(err));
 
-function getPosts() {
-  axios
-    .get("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => {
-      res.data.forEach((post) => {
-        postsDiv.innerHTML += `TITLE :  ${post.title} <br>    
-         <button onclick="editPost(${post.id})">Put</button>
-         <button onclick="deletePost(${post.id})">Delete</button>
-        `;
-      });
-    })
-    .catch((err) => console.error(err));
+async function getPosts() {
+  // axios
+  //   .get("https://jsonplaceholder.typicode.com/posts")
+  //   .then((res) => {
+  //     res.data.forEach((post) => {
+  //       postsDiv.innerHTML += `TITLE :  ${post.title} <br>
+  //        <button onclick="editPost(${post.id})">Put</button>
+  //        <button onclick="deletePost(${post.id})">Delete</button>
+  //       `;
+  //     });
+  //   })
+  //   .catch((err) => console.error(err));
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    res.data.forEach((post) => {
+      postsDiv.innerHTML += `TITLE :  ${post.title} <br>    
+           <button onclick="editPost(${post.id})">Put</button>
+           <button onclick="deletePost(${post.id})">Delete</button>
+          `;
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 // axios.get("")
 // axios.post("urlApi",{})
 
-function addPost() {
-  axios
-    .post("https://jsonplaceholder.typicode.com/posts", {
+async function addPost() {
+  // axios
+  //   .post("https://jsonplaceholder.typicode.com/posts", {
+  //     body: body.value,
+  //     title: "Me estas mareando mucho",
+  //   })
+  //   .then((res) => console.log(res))
+  //   .catch((err) => console.error(err));
+  try {
+    await axios.post("https://jsonplaceholder.typicode.com/posts", {
       body: body.value,
       title: "Me estas mareando mucho",
-    })
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function editPost(id) {
-  axios
-    .put("https://jsonplaceholder.typicode.com/posts/" + id, {
+async function editPost(id) {
+  // axios
+  //   .put("https://jsonplaceholder.typicode.com/posts/" + id, {
+  //     title: "Post updated",
+  //     body: body.value,
+  //   })
+  //   .then((res) => console.log(res))
+  //   .catch((err) => console.error(err));
+  try {
+    await axios.put("https://jsonplaceholder.typicode.com/posts/" + id, {
       title: "Post updated",
       body: body.value,
-    })
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function deletePost(id) {
-  axios
-    .delete("https://jsonplaceholder.typicode.com/posts/" + id)
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+async function deletePost(id) {
+  try {
+    const res = await axios.delete("https://jsonplaceholder.typicode.com/posts/" + id);
+    console.log(res)
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getBtn.addEventListener("click", getPosts);
